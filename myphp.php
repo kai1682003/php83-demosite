9129f8407b3dc9b12b9c0941067902108951d29f
kai1682003 <kai168@gmail.com>
2025-10-24 21:43:34 +0800

    Add basic PHP webpage with greeting and year

---
+++ myphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>我的PHP網頁</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "哈囉，這是一個PHP網頁！";
+        ?>
+    </h1>
+    <p>
+        <?php
+            $year = 2025;
+            echo "現在是 " . $year . " 年。";
+        ?>
+    </p>
+</body>
+</html>
